842070f7ae5118c6e84aaf1c6d5f04a5e61ea726
Dimitris Alexopoulos <glumfishfish@gmail.com>
2024-02-04 00:58:08 +0000

    Make map work

--- src/app.py
+++ src/app.py
@@ -15,6 +15,7 @@ import json
 if APP_ID == None and APP_KEY == None:
     print('APP_KEY and/or APP_ID has not been set.\nExiting...', file=sys.stderr)
     quit(128)
+
 from dash.dependencies import Input, Output, State
 
 server = Flask(__name__)
@@ -24,11 +25,11 @@ app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.LITERA
 app.config.suppress_callback_exceptions = True
 
 app.layout = html.Div([
-dbc.Row([
-    dbc.Col(
-        dcc.Graph(id="map_London")
-        #style={"width: 100vw, height: 100vh"}
+    dbc.Row([
+        dcc.Graph(id="map-london")],
+        style={"width": "100vw", "height": "100vh"}
     ),
+    dbc.Row(
     dbc.Col(
         dbc.Card([
             html.H5(children=["Travel Vs Cost"], className="text-center"),
@@ -36,7 +37,7 @@ dbc.Row([
             html.Div(id="slider-output-container")
         ])
     )
-], style={"width": "100vw", "height": "100vh"})
+    )
 ])
 
 @app.callback(Output("slider-output-container", "children"),
@@ -44,23 +45,28 @@ dbc.Row([
 def update_slider(value):
     return 'You have selected "{}"'.format(value)
 
+price_data = pandas.read_csv('assets/postcode_to_coord_and_price.csv')
+
 @app.callback(Output("map-london", "figure"),
               [Input("weight-slider", "value")])
-def update_map():
-    df = pandas.read_csv("assets/postcode_to_coord.csv")
+def update_map(value):
+    # df = pandas.read_csv("assets/postcode_to_coord.csv")
 
-    with open('london_boroughs.json', 'r') as f:
-        geoJson = json.load(f)
+    # with open('london_boroughs.json', 'r') as f:
+    #     geoJson = json.load(f)
 
     # fig = px.choropleth(
     #     df, geojson=geoJson, color = None, color_continuous_scale="Viridis", locations=None, scope="London", labels = {}
     # )
 
     #fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+    
+    maptooltip = {'Price': True, 'Longitude': False, 'Latitude': False}
 
-    fig = px.scatter(df, x="longitude", y="latitude")
+    map = px.scatter_mapbox(price_data, lat = 'Latitude', lon = 'Longitude', hover_name = 'Postcode', hover_data = maptooltip)
+    map.update_layout(mapbox = {'style': 'open-street-map'})
 
-    return fig
+    return map
 
 
 server = app.server
